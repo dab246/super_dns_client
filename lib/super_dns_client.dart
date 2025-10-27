20a91179786bb60d0319260dc789976edd409649
dab246 <tdvu@linagora.com>
2025-10-27 16:56:12 +0700

    fix: export DnsOverHttpsBinaryClient in main library file

--- lib/super_dns_client.dart
+++ lib/super_dns_client.dart
@@ -1,4 +1,6 @@
 export 'src/dns_client.dart';
 export 'src/dns_over_https.dart';
 export 'src/dns_record.dart';
+export 'src/dns_over_https_binary.dart';
+export 'src/dns_resolver.dart';
 export 'src/rr_type.dart';
